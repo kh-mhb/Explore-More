95b6c91c28984b57319ff46b373cd545773dfce2
Mahmudul Hasan <mahmudulhasansec65@gmail.com>
2023-04-10 22:55:52 +0600

    four card showing with button

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -18,22 +18,27 @@ const FeaturedJobs = () => {
     return (
         <div>
             <div className='text-center mt-3'>
-                <h1 className='font-semibold text-3xl mb-1' mt-5>Featured Jobs</h1>
+                <h1 className='font-semibold text-3xl mb-5 mt-10 '>Featured Jobs</h1>
                 <p className='font-thin text-base mb-3'>Explore thousands of job opportunities with all the information you need. Its your future </p>
             </div>
 
-            <div className='grid md:grid-cols-2 gap-6'>
+            <div className='grid md:grid-cols-2 gap-6 featured-job my-10'>
 
                 {
                     featuredJobs.map(featuredJob => <FeaturedJobList
                         key={featuredJob.id}
                         featuredJob={featuredJob}
+
                     >
 
 
                     </FeaturedJobList>)
                 }
 
+
+            </div>
+            <div className='text-center'>
+                <button className='btn my-6 w-40'>Show All</button>
             </div>
 
 
--- src/components/JobCategoryList/JobCateGoryList.jsx
+++ src/components/JobCategoryList/JobCateGoryList.jsx
@@ -19,7 +19,7 @@ const JobCateGoryList = () => {
                 <h3 className='font-semibold mt-3 text-2xl'>Job Category List</h3>
                 <p className='font-thin text-xs mt-1 mb-5'>Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
-            <div className='grid gap-6 justify-center items-center md:grid-cols-4 mx-0 my-auto '>
+            <div className='grid gap-6 justify-center items-center md:grid-cols-4 job-category '>
                 {
                     jobs.map(job => <JobCard
                         key={job.index}
