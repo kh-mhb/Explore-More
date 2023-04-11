57c4b892cde4daa669549e4040d0386c5113cc3a
Mahmudul Hasan <mahmudulhasansec65@gmail.com>
2023-04-12 05:24:55 +0600

    ui design done for applid job page

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -3,9 +3,25 @@ import './AppliedJobs.css';
 
 const AppliedJobs = () => {
     return (
-        <div>
-            <h1>I am from applied jobs</h1>
-        </div>
+        <div className='root-div p-10 '>
+            <div className="card card-side shadow-lg img-part">
+                <img src="https://st2.depositphotos.com/3758943/6040/i/450/depositphotos_60400957-stock-photo-the-man-in-the-office.jpg" alt="Company_Name" />
+                <div className="card-body">
+                    <h2 className="card-title">Here is Job Tittle</h2>
+                    <p className='text-xs font-thin'>Here is company name</p>
+                    <div className='btn-grp'>
+                        <button className='text-left font-mono'>Remote</button>
+                        <button className='text-left font-mono'>Online</button>
+                        <button className='text-right font-mono bg-warning hover:bg-yellow-100' style={{ position: 'absolute', top: '40%', right: '40px' }}>View Details</button>
+                    </div>
+                    <div className='flex gap-2'>
+                        <h1>Salary</h1>
+                        <h2>Location</h2>
+                    </div>
+                </div>
+            </div>
+
+        </div >
     );
 };
 
