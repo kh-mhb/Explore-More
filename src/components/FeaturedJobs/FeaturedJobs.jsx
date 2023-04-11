3444a90b961be125571df9c59da0e67c54b2359f
Mahmudul Hasan <mahmudulhasansec65@gmail.com>
2023-04-11 19:03:36 +0600

    featured job jsx file modified for show all button

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,47 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import './FeaturedJobs.css';
-import FeaturedJobList from '../FeaturedJobList/FeaturedJobList';
+import React, { useEffect, useState } from "react";
+import "./FeaturedJobs.css";
+import FeaturedJobList from "../FeaturedJobList/FeaturedJobList";
 
 const FeaturedJobs = () => {
-
     let [featuredJobs, setFeaturedJobs] = useState([]);
+    const [disabled, setDisabled] = useState(false);
     // console.log(featuredJobs);
     useEffect(() => {
-
-        fetch('data.json')
-            .then(res => res.json())
-            .then(data => setFeaturedJobs(data));
-
+        fetch("data.json")
+            .then((res) => res.json())
+            .then((data) => setFeaturedJobs(data.slice(0, 4)));
     }, []);
-
+    const handlerToShowAll = () => {
+        fetch("data.json")
+            .then((res) => res.json())
+            .then((data) => setFeaturedJobs(data));
+        setDisabled(true);
+    };
 
     return (
         <div>
-            <div className='text-center mt-3'>
-                <h1 className='font-semibold text-3xl mb-5 mt-10 '>Featured Jobs</h1>
-                <p className='font-thin text-base mb-3'>Explore thousands of job opportunities with all the information you need. Its your future </p>
+            <div className="text-center mt-3">
+                <h1 className="font-semibold text-3xl mb-5 mt-10 ">Featured Jobs</h1>
+                <p className="font-thin text-base mb-3">
+                    Explore thousands of job opportunities with all the information you
+                    need. Its your future{" "}
+                </p>
             </div>
 
-            <div className='grid md:grid-cols-2 gap-6 featured-job my-10'>
-
-                {
-                    featuredJobs.map(featuredJob => <FeaturedJobList
+            <div className="grid md:grid-cols-2 gap-6 featured-job my-10">
+                {featuredJobs.map((featuredJob) => (
+                    <FeaturedJobList
                         key={featuredJob.id}
                         featuredJob={featuredJob}
-
-                    >
-
-
-                    </FeaturedJobList>)
-                }
-
-
+                    ></FeaturedJobList>
+                ))}
             </div>
-            <div className='text-center'>
-                <button className='btn btn-warning my-6 w-40'>Show All</button>
+            <div className="text-center">
+                <button
+                    className={`btn btn-warning my-6 w-40 ${disabled ? "hidden" : ""} `}
+                    onClick={handlerToShowAll}
+                >
+                    Show All
+                </button>
             </div>
-
-
         </div>
     );
 };
